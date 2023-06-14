89af7422e3937a82f3ebdf7cb55bffe650b1dfc2
d1stuzione <elanderson331@outlook.com>
2023-06-14 18:31:14 -0400

    added some variables / look at comments

--- backend.py
+++ backend.py
@@ -9,10 +9,25 @@ url = f'https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=ye
 
 response = requests.get(url)
 
+
+
 if response.status_code == 200:
     data = response.json()
-    print(data['current']['condition']['text'])
-    print(data['location']['lon'])
-    print(data['location']['lat'])
+    
+    #idk where to put these but I defined the "most important" statistics and put them to vairables from the dict
+    #youll have to show me how to organize it efficiently
+
+    condition = (data['current']['condition']['text'])
+    lon = (data['location']['lon'])
+    lat = (data['location']['lat'])
+    localtime = (data['location']['localtime'])
+    temp_f = (data['current']['temp_f'])
+    temp_c = (data['current']['temp_c'])
+    
+    print(condition)
+    print(localtime)
+    print(temp_f)
+    print(temp_c)
+
 else:
     print('Error fetching weather data')
